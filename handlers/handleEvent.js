04a4f12f0ccb49f982b873687de79ed7ca3d9a07
Ahmed Amine Boustani <ahmed.a.boustani@gmail.com>
2017-08-24 16:12:24 +0100

    :bug:

--- handlers/handleEvent.js
+++ handlers/handleEvent.js
@@ -1,17 +1,18 @@
 const _ = require('../messages/generic')
 
+const token = process.env.APP_ACCESS_TOKEN
+
 module.exports = (event) => {
     const sender = event.sender.id
     if (event.message && event.message.text) {
         let text = event.message.text
         if (text === 'Generic') {
             _.sendGenericMessage(sender)
-            continue
+            return
         }
     }
     if (event.postback) {
         let text = JSON.stringify(event.postback)
         sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-        continue
     }
 }
